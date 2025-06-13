2c706d7eae939cc6c6b652d3e252447f4e5c09ee
WojFilipe <filip.wojtysiak803@gmail.com>
2025-06-13 20:10:10 +0200

    Projekt

--- 20250315_example/Cdv.PeopleAPI/obj/Debug/net9.0/Cdv.PeopleAPI.AssemblyInfo.cs
+++ 20250315_example/Cdv.PeopleAPI/obj/Debug/net9.0/Cdv.PeopleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cdv.PeopleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7902037b6d894cddfd5eb89ba290fc79cc1a9406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cdv.PeopleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cdv.PeopleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
